0a9741b433f46a276f2c4b5077ba21d34bea55b3
Ankit Pal <ankitpal59911@gmail.com>
2026-08-15 22:15:15 +0530

    Add files via upload
    Arithmetic Operations Program

---
+++ Arithmetic Pro.py
@@ -0,0 +1,27 @@
+a = 40
+b = 20
+print(type (a))
+print(type (b))
+sum = a+b
+print(sum)
+diff = a-b
+print(diff)
+multiply = a*b
+print(multiply)
+divison = a/b
+print(divison)
+module = a%b
+print(module)
+print("\n")
+a=float(40)
+b=float(20)
+sum=a+b
+print(sum)
+diff=a-b
+print(diff)
+multiply=a*b
+print(multiply)
+divison=a/b
+print(divison)
+module=a%b
+print(module)
