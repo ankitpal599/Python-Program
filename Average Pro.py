37744bd087baf81dcc0148a44196d3d24fda1036
Ankit Pal <ankitpal59911@gmail.com>
2026-08-16 21:09:49 +0530

    Add files via upload
    Python Program to find average of six numbers whose value can be integer or decimal number.

---
+++ Average Pro.py
@@ -0,0 +1,13 @@
+first = 12
+second = 24.2
+third = 36
+fourth = 48.4
+fifth = 60
+print("average=",first+second+third+fourth+fifth/2,"\n")
+
+first = int(input("enter first:"))
+second = int(input("enter second:"))
+third = float(input("enter third:"))
+fourth = float(input("enter fourth:"))
+fifth = int(input("enter fifth:"))
+print("average=",first+second+third+fourth+fifth/2)
