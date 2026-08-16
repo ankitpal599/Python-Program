c532e16d960c60fdc15fb92b140dd8a1f268199a
Ankit Pal <ankitpal59911@gmail.com>
2026-08-16 21:06:58 +0530

    Add files via upload
    Python Program to find perimeter and area of Square & Rectangle respectively by entering input values.

---
+++ Area & Perimeter Pro.py
@@ -0,0 +1,9 @@
+side = int(input("enter square side:"))
+#we can use float instead of int for decimal number.
+print("area=",side*side)
+print("perimeter=",4*side,"\n")
+length = int(input("enter length:"))
+breadth = int(input("enter breadth:"))
+#we can use float instead of int if any of the both no. are decimal number.
+print("area=",length*breadth)
+print("perimeter=",2*length+breadth)
