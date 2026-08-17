c27851b987d5d537ad111403da58eebf0d914c19
Ankit Pal <ankitpal59911@gmail.com>
2026-08-17 22:24:34 +0530

    Add files via upload
    Python program to demonstrate different string operations.

---
+++ String Pro.py
@@ -0,0 +1,13 @@
+Name = input("enter your  name:")
+print("length of your name is", len(Name),"\n")
+str = "I am currently pursuing BE fron CSE"
+print(str.endswith("SE"))
+print(str.endswith("am"))
+str = "i belong to Ayodhya"
+print(str.capitalize())
+str = "I am currently living in Delhi"
+print(str.replace("Delhi","Agra"))
+str = "I am currently in first year"
+print(str.find("currently"))
+str = "I am learning Python and preparing for DSA"
+print(str.count("a"))
