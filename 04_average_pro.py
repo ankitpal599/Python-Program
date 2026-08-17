095773f1d047ff822c129354d9beef3e57cced4f
Ankit Pal <ankitpal59911@gmail.com>
2026-08-17 22:41:17 +0530

    Update 04_average_pro.py

--- 04_average_pro.py
+++ 04_average_pro.py
@@ -1,13 +1,14 @@
+#Python program to calculate average of given numbers and also taking input values from user.
 first = 12
 second = 24.2
 third = 36
 fourth = 48.4
 fifth = 60
-print("average=",first+second+third+fourth+fifth/2,"\n")
+print("average=",first+second+third+fourth+fifth/5,"\n")
 
 first = int(input("enter first:"))
 second = int(input("enter second:"))
 third = float(input("enter third:"))
 fourth = float(input("enter fourth:"))
 fifth = int(input("enter fifth:"))
-print("average=",first+second+third+fourth+fifth/2)
+print("average=",first+second+third+fourth+fifth/5)
