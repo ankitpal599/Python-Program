f11e03c509eac08dc4b1243233edc6ecdc70be55
Ankit Pal <ankitpal59911@gmail.com>
2026-08-18 22:37:16 +0530

    Update 07_odd_and_even_pro.py

--- 07_odd_and_even_pro.py
+++ 07_odd_and_even_pro.py
@@ -1,12 +1,8 @@
+#Python program to check odd and even number by taking input by user.
 number = int(input("enter the number:"))
 remainder = number%2
 if(remainder == 0 ):
     print("Even number")
 else:
     print("Odd number","\n")    
-number = float(input("enter the number:"))
-remainder = number%2
-if(remainder == 0 ):
-    print("Even number")
-else:
-    print("Odd number")    
+ 
