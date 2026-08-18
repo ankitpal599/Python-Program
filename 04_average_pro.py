7960fea9d69f9184e860511829038cb02bee4769
Ankit Pal <ankitpal59911@gmail.com>
2026-08-18 23:15:52 +0530

    Update 04_average_pro.py

--- 04_average_pro.py
+++ 04_average_pro.py
@@ -4,11 +4,11 @@ second = 24.2
 third = 36
 fourth = 48.4
 fifth = 60
-print("average=",first+second+third+fourth+fifth/5,"\n")
+print("average=",(first+second+third+fourth+fifth)/5,"\n")
 
 first = int(input("enter first:"))
 second = int(input("enter second:"))
 third = float(input("enter third:"))
 fourth = float(input("enter fourth:"))
 fifth = int(input("enter fifth:"))
-print("average=",first+second+third+fourth+fifth/5)
+print("average=",(first+second+third+fourth+fifth)/5)
