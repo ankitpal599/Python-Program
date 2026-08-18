13161b59d2e1d59bc0f5f89aa8e21d7fcba9ae1a
Ankit Pal <ankitpal59911@gmail.com>
2026-08-18 22:38:33 +0530

    Add files via upload

---
+++ 08_largest_number_pro.py
@@ -0,0 +1,9 @@
+num1 = int(input("enter first number:"))
+num2 = int(input("ener second number:"))
+num3 = int(input("enter third number:"))
+if(num1 >= num2 and num1 >= num3):
+    print("first number is largest",num1)
+elif(num2 >= num3):
+    print("second number is largest",num2)
+else:
+    print("third number is largest",num3)
