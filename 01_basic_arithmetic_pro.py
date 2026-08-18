bff8cdfddf5c1610c831cb36b20e49a9c1f2d429
Ankit Pal <ankitpal59911@gmail.com>
2026-08-18 23:16:54 +0530

    Update 01_basic_arithmetic_pro.py

--- 01_basic_arithmetic_pro.py
+++ 01_basic_arithmetic_pro.py
@@ -3,14 +3,14 @@ a = 40
 b = 20
 print(type (a))
 print(type (b))
-sum = a+b
-print(sum)
+add = a+b
+print(add)
 diff = a-b
 print(diff)
 multiply = a*b
 print(multiply)
-divison = a/b
-print(divison)
+division = a/b
+print(division)
 module = a%b
 print(module)
 print("\n")
