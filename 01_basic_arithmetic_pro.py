d1804a00f50bc442556df0ceba38fe8d2e6c0a17
Ankit Pal <ankitpal59911@gmail.com>
2026-08-18 23:24:34 +0530

    Update 01_basic_arithmetic_pro.py

--- 01_basic_arithmetic_pro.py
+++ 01_basic_arithmetic_pro.py
@@ -12,17 +12,4 @@ print(multiply)
 division = a/b
 print(division)
 module = a%b
-print(module)
-print("\n")
-a=float(40)
-b=float(20)
-add =a+b
-print(add)
-diff=a-b
-print(diff)
-multiply=a*b
-print(multiply)
-division=a/b
-print(division)
-module=a%b
-print(module)
+print(module)
