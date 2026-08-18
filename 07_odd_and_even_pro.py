c3636828a2418f038d548be390ad076e7243fb97
Ankit Pal <ankitpal59911@gmail.com>
2026-08-18 22:34:43 +0530

    Add files via upload

---
+++ 07_odd_and_even_pro.py
@@ -0,0 +1,12 @@
+number = int(input("enter the number:"))
+remainder = number%2
+if(remainder == 0 ):
+    print("Even number")
+else:
+    print("Odd number","\n")    
+number = float(input("enter the number:"))
+remainder = number%2
+if(remainder == 0 ):
+    print("Even number")
+else:
+    print("Odd number")    
