8e474b3cd6033565b12801bb885f0f827808badf
Ankit Pal <ankitpal59911@gmail.com>
2026-08-19 21:47:58 +0530

    Add files via upload

---
+++ 10_nesting_pro.py
@@ -0,0 +1,8 @@
+age = int(input("enter the age:"))
+if(age >= 18):
+    if(age > 35):
+        print("not eligible for IAS exam")
+    else:
+        print("eligible for IAS exam" )
+else:
+    print("not eligible for IAS exam")
