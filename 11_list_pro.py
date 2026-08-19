2af2c37ca7fb02b1e007ece4b912119039d96227
Ankit Pal <ankitpal59911@gmail.com>
2026-08-19 21:48:17 +0530

    Add files via upload

---
+++ 11_list_pro.py
@@ -0,0 +1,18 @@
+evenlist = [2, 4, 6, 8, 10, 12, 14]
+print(evenlist[1:4])
+print(evenlist[-3:-1])
+
+oddlist = [3, 1, 5, 9, 7, 11, 13]
+oddlist.append(15)
+print(oddlist)
+print(oddlist.sort())
+print(oddlist)
+print(oddlist.sort(reverse=True))
+print(oddlist)
+oddlist.reverse()
+print(oddlist)
+oddlist.insert(2,15)
+print(oddlist)
+oddlist.remove(3)
+print(oddlist)
+
