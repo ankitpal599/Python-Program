f8e0e20050963ebc23d59e7614d2b812043be5df
Ankit Pal <ankitpal59911@gmail.com>
2026-08-20 20:58:34 +0530

    Add files via upload

---
+++ 12_subject_pro.py
@@ -0,0 +1,8 @@
+subjects = []
+sub1 = input("enter 1st sub1: ")
+subjects.append(sub1)
+sub2 = input("enter 2nd sub2:")
+subjects.append(sub2)
+sub3 = input("enter 3rd sub3: ")
+subjects.append(sub3)
+print(subjects)
