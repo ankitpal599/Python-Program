cc5d2ad47ce202cb3596f3ddb9281d44c89c9d96
Ankit Pal <ankitpal59911@gmail.com>
2026-08-20 21:04:30 +0530

    Add files via upload

---
+++ 14_palindrome_pro.py
@@ -0,0 +1,15 @@
+list1 = [1,2,3,4,3,2,1]
+copy_list1 = list1.copy()
+copy_list1.reverse()
+if(copy_list1 == list1):
+    print("palindrome")
+else:
+    print("NOT a palindrome")
+    
+list2 = [1,3,2,5,4,7,6]
+copy_list2 = list2.copy()
+copy_list2.reverse()
+if(copy_list2 == list2):
+    print("palindrome")
+else:
+    print("NOT a palindrome")
