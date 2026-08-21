27ec925cba8dc4fafde186f742718b1bfd0d28a5
Ankit Pal <ankitpal59911@gmail.com>
2026-08-21 21:42:05 +0530

    Add files via upload

---
+++ 15_dictionary_operations_pro.py
@@ -0,0 +1,19 @@
+dict = {
+    "name" : "Ankit Pal",
+    "class" : "12th",
+    "subject & marks" : {
+        "Hindi" : 90,
+        "English" : 86,
+        "Math" : 70,
+        "Physics" : 75,
+        "Chemistry" : 78,
+    } 
+}
+print(dict)
+print(type(dict),"\n")
+print(dict.keys())
+print(dict.values())
+print(dict.items())
+print(dict.get("name"))
+dict.update({"city" : "Ayodhya"})
+print(dict)
