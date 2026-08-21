b5b0d772c66f4d3f45dae81c4f298c963bf1dd91
Ankit Pal <ankitpal59911@gmail.com>
2026-08-21 21:43:33 +0530

    Add files via upload

---
+++ 17_dic_subject_pro.py
@@ -0,0 +1,8 @@
+marks = {}
+x = int(input("enter physics :"))
+marks.update({"physics": x})
+y = int(input("enter chemistry :"))
+marks.update({"chemistry": y})
+z = int(input("enter maths :"))
+marks.update({"maths": z})
+print(marks)
