c6cdb584283fedc9125d6ae12673baf66c25f513
Ankit Pal <ankitpal59911@gmail.com>
2026-08-21 21:49:18 +0530

    Update 17_dic_subject_pro.py

--- 17_dic_subject_pro.py
+++ 17_dic_subject_pro.py
@@ -1,3 +1,4 @@
+#Python program to create an empty dictionary and to enter marks of 3 subjects from the user & store them in a dicitionary.
 marks = {}
 x = int(input("enter physics :"))
 marks.update({"physics": x})
