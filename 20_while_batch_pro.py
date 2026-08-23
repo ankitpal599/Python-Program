361a2663322cf175a6ba16b13c0a59fca6d1ab5b
Ankit Pal <ankitpal59911@gmail.com>
2026-08-23 22:16:30 +0530

    Add files via upload

---
+++ 20_while_batch_pro.py
@@ -0,0 +1,6 @@
+students = ["Ankit", "Abhinav", "Arti", "Aditi", "Aditya", "Ambrish", "Diksha", "Deepak", "Dev", "Govind", "Garima", "Hemant"]
+x = "Diksha" 
+idx = 0
+while idx < len(students):
+    print(students[idx])
+    idx += 1
