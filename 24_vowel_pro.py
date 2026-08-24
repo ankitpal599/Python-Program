f188e5c1f9d26ab8a000fa52da6119a72eb832f3
Ankit Pal <ankitpal59911@gmail.com>
2026-08-24 22:27:09 +0530

    Update 24_vowel_pro.py

--- 24_vowel_pro.py
+++ 24_vowel_pro.py
@@ -1,3 +1,4 @@
+#Python program to take a sentence as input from the user,find & print all vowels in the sentence and count the total no of vowels using for loop. 
 sentence = input("enter a sentence:")
 count = 0
 for char in sentence:
