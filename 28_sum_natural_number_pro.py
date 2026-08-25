0ea09a94cff1eeaff719c5c175ed66386e769780
Ankit Pal <ankitpal59911@gmail.com>
2026-08-25 22:25:06 +0530

    Add files via upload

---
+++ 28_sum_natural_number_pro.py
@@ -0,0 +1,12 @@
+n = 6
+sum = 0
+i = 1
+while i <= n:
+    sum += i
+    i += 1
+print("total sum =", sum, ("\n"))
+n = 8
+sum = 0
+for i in range(1, n + 1):
+    sum += i
+print("total sum =", sum)
