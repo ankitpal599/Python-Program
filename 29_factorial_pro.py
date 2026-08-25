cdbf0d38881f6fb5ed1d77549f0ee064a7899b25
Ankit Pal <ankitpal59911@gmail.com>
2026-08-25 22:25:55 +0530

    Add files via upload

---
+++ 29_factorial_pro.py
@@ -0,0 +1,12 @@
+n = 4
+fact = 1
+i = 1
+while i <= n:
+    fact *= i
+    i += 1
+print("factoial =", fact, ("\n"))
+n = 5
+fact = 1
+for i in range(1, n+1):
+    fact *= i
+print("factorial =", fact)
