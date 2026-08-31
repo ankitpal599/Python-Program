364f0422e428b38075d691b776055aac8c1da9b6
Ankit Pal <ankitpal59911@gmail.com>
2026-08-31 20:01:57 +0530

    Add files via upload

---
+++ 30_basic_function_pro.py
@@ -0,0 +1,20 @@
+def calc_sum(a, b):
+    sum = a + b
+    print(sum)
+    return sum
+calc_sum(12, 44)
+calc_sum(18, 64)
+
+def calc_prod(a, b):
+    print(a * b)
+    return a * b
+calc_prod(4, 16)
+calc_prod(12, 10)
+
+def calc_avg(a, b, c):
+    sum = a + b + c
+    avg = sum/3
+    print(avg)
+    return avg
+calc_avg(12, 16, 20)
+calc_avg(4, 6, 8)
