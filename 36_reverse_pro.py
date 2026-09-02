7d0750d2d2980084af570d13dbb34b91481a90c6
Ankit Pal <ankitpal59911@gmail.com>
2026-09-02 20:24:56 +0530

    Add files via upload

---
+++ 36_reverse_pro.py
@@ -0,0 +1,9 @@
+num = int(input("enter a number:"))
+def reverse_number(n):
+    rev = 0
+    while n > 0:
+        digit = n % 10
+        rev = rev * 10 + digit
+        n = n//10
+    return rev
+print("reverse =",reverse_number(num))
