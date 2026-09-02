43e5f92c0bd7d5264ce25a603d322501ba5cb651
Ankit Pal <ankitpal59911@gmail.com>
2026-09-02 20:31:24 +0530

    Update 35_fibonacci_pro.py

--- 35_fibonacci_pro.py
+++ 35_fibonacci_pro.py
@@ -1,7 +1,8 @@
+#Python program to print Fibonacci series.
+n = int(input("enter the number term:"))
 def fibonacci(n):
     if n <= 1:
         return n
     return fibonacci(n - 1) + fibonacci(n - 2)
-n = int(input("enter the number term:"))
 for i in range (n):
     print(fibonacci(i), end="")
