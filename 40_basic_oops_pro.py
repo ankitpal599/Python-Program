66365971e5f4416a3ef1b7f53923b1dcd5dcec6c
Ankit Pal <ankitpal59911@gmail.com>
2026-09-13 21:55:08 +0530

    Add files via upload

---
+++ 40_basic_oops_pro.py
@@ -0,0 +1,13 @@
+class Student:
+    def __init__(self, name, age, marks):
+        self.name = name
+        self.age = age
+        self.marks = marks
+
+    def display(self):
+     print("Name:",self.name)
+     print("Age:",self.age)
+     print("Marks:",self.marks)
+
+student1 = Student("Ankit", 19, 85)
+student1.display()
