166c4f4a84f1175e30102c6656baf91c050994fc
Ankit Pal <ankitpal59911@gmail.com>
2026-09-13 21:55:33 +0530

    Add files via upload

---
+++ 41_oops_account_pro.py
@@ -0,0 +1,27 @@
+class Account:
+    def __init__(self, bal, acc):
+        self.balance = bal
+        self.account_no = acc
+
+    def debit(self, amount):
+        self.balance -= amount
+        print("Rs.", amount, "was debited")
+        print("total balance = ", self.get_balance())
+
+    def credit(self, amount):
+        self.balance += amount
+        print("Rs.", amount, "was credited")
+        print("total balance = ", self.get_balance())
+
+    def get_balance(self):
+        return self.balance
+
+acc1 = Account(20000, 244678)
+acc1.debit(5000)
+acc1.credit(10000)
+acc1.debit(20000)
+acc1.credit(30000)
+
+
+
+        
